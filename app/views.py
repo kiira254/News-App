addcd059d4b69d2a40843369624e90c86cf9d53d
kiira254 <nkamotho57@gmail.com>
2020-09-11 20:28:02 +0300

    add for

--- app/views.py
+++ app/views.py
@@ -1,16 +1,20 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # views
-@app.route('/news/<int:news_id>') 
+@app.route('/') 
 
 def index():
 
     '''
     view root page function that returns the index page and its data
     '''
+    # Getting popular news
+    popular_news = get_news('popular')
+    print(popular_news)
     title='Home- Welcome to the best news Review Website'
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title popular=popular_news)
 def news(news_id):
 
     '''
