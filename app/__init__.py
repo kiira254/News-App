9e5f9e960b17a4e2f973efadffb38b520a405028
kiira254 <nkamotho57@gmail.com>
2020-09-11 18:08:46 +0300

    add debug change

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 #initializing application
-app = Flask(__name__,isinstance_relative_config = True)
+app = Flask(__name__,instance_relative_config = True)
 
 # setting up configuration
 app.config.from_object(DevConfig)
