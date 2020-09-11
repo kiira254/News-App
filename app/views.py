2ad887ad7a2229af88f6dd55e4dffc77d58870c2
kiira254 <nkamotho57@gmail.com>
2020-09-11 16:26:25 +0300

    add loop

--- app/views.py
+++ app/views.py
@@ -2,10 +2,21 @@ from flask import render_template
 from app import app
 
 # views
-@app.route('/newspaper/<int:newspaper_id>') 
-def newspaper(newspaper_id):
+@app.route('/news/<int:news_id>') 
+
+def index()
 
     '''
     view root page function that returns the index page and its data
     '''
-    return render_template('newspaper.html', id= newspaper_id)
+    title='Home- Welcome to the best news Review Website'
+    return render_template('index.html',title=title)
+def news(news_id):
+
+    '''
+    view root page function that returns the index page and its data
+    '''
+    news_id='Home- List of News channels'
+    return render_template('news.html', id= news_id)
+
+
