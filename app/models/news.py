a0b06d3de374f2802725e1bad8dbe7f61322fdd9
kiira254 <nkamotho57@gmail.com>
2020-09-11 19:05:27 +0300

    add request

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    News Class to define News Objects
+    '''
+    def __init__(self,id,name,author,title,desciption,poster,content):
+        self.id == id
+        self.name ==name
+        self.author== author
+        self.title== title
+        self.description== desciption
+        self.poster == 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.content ==  content
+        
+
--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+from urllib.request,json
+from .models import news
+
+News= news.News
 
 # getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#  getting the news base url
+base_url= app.config['NEWS_API_BASE_URL']
