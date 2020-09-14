dde2490443791ae3f9b2c574637f2070a23960ca
kiira254 <nkamotho57@gmail.com>
2020-09-14 08:26:00 +0300

    UPDATE config

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL='http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-11&sortBy=publishedAt&apiKey={}'
+    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
+    SECRET_KEY= os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -22,3 +26,7 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+Config_options= {
+    'development': DevConfig,
+    'production' : ProdConfig
+}
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,3 @@
 NEWS_API_KEY = 'f686a2bec46142f588d667a0c2cd0f19' 
+NEWS_API_KEY= '<NEWS_API_KEY>'
 SECRET_KEY = '<Flask WTF Secret Key>'
