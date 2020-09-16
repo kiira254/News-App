8430db4ac92eb4ddfee5274763e190ff5a2772a2
kiira254 <nkamotho57@gmail.com>
2020-09-16 10:09:57 +0300

    add changes

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export NEWS_API_KEY=f686a2bec46142f588d667a0c2cd0f19
-export SECRET_KEY=f686a2bec46142f588d667a0c2cd0f19
+export NEWS_API_KEY= 'f686a2bec46142f588d667a0c2cd0f19'
+export SECRET_KEY='Flask WTF Secret Key'
+
 
 python manage.py server
